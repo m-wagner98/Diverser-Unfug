416f78360bfe16efaf1c2b4d2c2f3582f8b8d512
Martin Wagner <48804848+m-wagner98@users.noreply.github.com>
2021-02-09 16:07:16 +0100

    Update häck.cpp

--- Verschiedenes/häck.cpp
+++ Verschiedenes/häck.cpp
@@ -1,24 +1,36 @@
+// Written by Ted Burke - last updated 17-4-2012
+// Adjusted by Martin Wagner
+// Because the SendInput function is only supported in
+// Windows 2000 and later, WINVER needs to be set as
+// follows so that SendInput gets defined when windows.h
+// is included below.
+#define WINVER 0x0500
 #include <windows.h>
 
-int main(void) {
-    int x,y, counter;
-    x = 5;
-    y = 5;
-    while(true) {
-        counter = 0;
-        while (counter < 150) {
-          // bisschen die Maus bewegen...
-          x += 5;
-          y += 5;
-          SetCursorPos(x,y);
-          Sleep(50);
-          counter ++;
-        }
-        // Cursor an ursprüngliche Position...
-        x = 5;
-        y = 5;
-        SetCursorPos(x,y);
-        Sleep(10000);
-    }
-    return 0;
+int main()
+{
+
+  while (true) {
+    // This structure will be used to create the keyboard
+	  // input event.
+	  INPUT ip;
+
+	  // Set up a generic keyboard event.
+	  ip.type = INPUT_KEYBOARD;
+	  ip.ki.wScan = 0; // hardware scan code for key
+	  ip.ki.time = 0;
+	  ip.ki.dwExtraInfo = 0;
+
+	  // Press the "A" key
+	  ip.ki.wVk = 0x41; // virtual-key code for the "a" key
+	  ip.ki.dwFlags = 0; // 0 for key press
+	  SendInput(1, &ip, sizeof(INPUT));
+
+	  // Release the "A" key
+	  ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+	  SendInput(1, &ip, sizeof(INPUT));
+    Sleep(5000);
+  }
+  
+	return 0;
 }
