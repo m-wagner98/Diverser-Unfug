bf6ef64580132558fe303309fd211bf8ff606d18
Martin Wagner <48804848+m-wagner98@users.noreply.github.com>
2021-02-09 13:04:11 +0100

    Add files via upload

---
+++ Verschiedenes/häck.cpp
@@ -0,0 +1,14 @@
+﻿#include <windows.h>
+
+int main(void) {
+    int x,y;
+    x = 10;
+    y = 10;
+    while(true) {
+        SetCursorPos(x,y);
+        x += 1;
+        y += 1;
+        Sleep(2000);
+    }
+    return 0;
+}
