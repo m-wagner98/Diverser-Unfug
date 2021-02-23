42d35f05cd5f3773176c9a4b6db7446c8ae0b104
Martin Wagner <48804848+m-wagner98@users.noreply.github.com>
2021-02-23 01:40:22 +0100

    Update häck.cpp

--- Verschiedenes/häck.cpp
+++ Verschiedenes/häck.cpp
@@ -12,25 +12,25 @@ int main()
 
   while (true) {
     // This structure will be used to create the keyboard
-	  // input event.
-	  INPUT ip;
+    // input event.
+    INPUT ip;
 
-	  // Set up a generic keyboard event.
-	  ip.type = INPUT_KEYBOARD;
-	  ip.ki.wScan = 0; // hardware scan code for key
-	  ip.ki.time = 0;
-	  ip.ki.dwExtraInfo = 0;
+    // Set up a generic keyboard event.
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.wScan = 0; // hardware scan code for key
+    ip.ki.time = 0;
+    ip.ki.dwExtraInfo = 0;
 
-	  // Press the "A" key
-	  ip.ki.wVk = 0x41; // virtual-key code for the "a" key
-	  ip.ki.dwFlags = 0; // 0 for key press
-	  SendInput(1, &ip, sizeof(INPUT));
+    // Press the "A" key
+    ip.ki.wVk = 0x41; // virtual-key code for the "a" key
+    ip.ki.dwFlags = 0; // 0 for key press
+    SendInput(1, &ip, sizeof(INPUT));
 
-	  // Release the "A" key
-	  ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
-	  SendInput(1, &ip, sizeof(INPUT));
-    	  Sleep(5000);
+    // Release the "A" key
+    ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+    SendInput(1, &ip, sizeof(INPUT));
+    Sleep(5000);
   }
   
-	return 0;
+  return 0;
 }
